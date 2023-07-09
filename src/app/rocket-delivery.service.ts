12efc9bcc37cdbe66a47a8c0556e4b1c9ee8a37b
dubraska31 <dubraskaprimi31@gmail.com>
2023-07-09 01:35:51 -0300

    improve token

--- src/app/rocket-delivery.service.ts
+++ src/app/rocket-delivery.service.ts
@@ -9,13 +9,12 @@ import { Menu } from "./menu";
 export class RocketDeliveryService {
 
     private rocketDeliveryUrl = 'http://localhost:8080/api/';  // URL to web api
+    private token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJhZG1pbiJ9.PRTaW4zUSHd5cWnQxsRcdnue5FBc3mtijvnELrPWYPGQDDhrCWLsvJUHmGXnp2vLheztEA5yzpzfovPe8Cwgcn7eK939kuc_RU87kk15OGruWmTa8Qz_Ztu1CXWB-0SbklTbPxQ26XjP4Vk3i4TttjKdLNWMJdw4X2HEjCzRK97I06ThjspffXiiSEgXe9TSfALS5V_WaWpEb2mjRNCO10lPaG7Wd1rzyotV92LuYPpxgqPjJn0woGAVFWG2jxy8rjfSSg-7qdW6BlriuwPUtfkFGfpGKf9gJG1xRuVuONJ0ix5dzJashL2KcwUI9qGcxICqPe1iVjtfZirYjBDX4A'
 
     httpOptions = {
         headers: new HttpHeaders({
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJhZG1pbiJ9.PRTaW4zUSHd5cWnQxsRcdnue5FBc3mtijvnELrPWYPGQDDhrCWLsvJUHmGXnp2vLheztEA5yzpzfovPe8Cwgcn7eK939kuc_RU87kk15OGruWmTa8Qz_Ztu1CXWB-0SbklTbPxQ26XjP4Vk3i4TttjKdLNWMJdw4X2HEjCzRK97I06ThjspffXiiSEgXe9TSfALS5V_WaWpEb2mjRNCO10lPaG7Wd1rzyotV92LuYPpxgqPjJn0woGAVFWG2jxy8rjfSSg-7qdW6BlriuwPUtfkFGfpGKf9gJG1xRuVuONJ0ix5dzJashL2KcwUI9qGcxICqPe1iVjtfZirYjBDX4A',
-            'Access-Control-Allow-Origin': 'http://localhost:4200',
-            'Access-Control-Allow-Credentials': 'true',
+            'Authorization': 'Bearer ' + this.token
         })
     };
 
