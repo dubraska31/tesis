8a4fc42ca1393043b1af58067aa7194e997fbeb1
dubraska31 <dubraskaprimi31@gmail.com>
2023-07-13 00:28:02 -0300

    indent

--- src/app/comidas/comidas.component.ts
+++ src/app/comidas/comidas.component.ts
@@ -7,17 +7,16 @@ import { RocketDeliveryService } from '../rocket-delivery.service';
 @Component({
   selector: 'app-comidas',
   templateUrl: './comidas.component.html',
-  styleUrls: ['./comidas.component.css']
+  styleUrls: ['./comidas.component.css'],
 })
 export class ComidasComponent {
-
   comidas: Hero[] = [];
   menus: Menu[] = [];
 
   constructor(
     private heroService: HeroService,
     private rocketDeliveryService: RocketDeliveryService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     // this.getComidas();
@@ -25,31 +24,34 @@ export class ComidasComponent {
   }
 
   getMenus(): void {
-    this.rocketDeliveryService.getMenus()
-      .subscribe(menus => this.menus = menus);
+    this.rocketDeliveryService
+      .getMenus()
+      .subscribe((menus) => (this.menus = menus));
   }
 
   getComidas(): void {
-    this.heroService.getHeroes()
-      .subscribe(comidas => this.comidas = comidas);
+    this.heroService
+      .getHeroes()
+      .subscribe((comidas) => (this.comidas = comidas));
   }
 
   add(name: string): void {
     name = name.trim();
-    if (!name) { return; }
-    this.heroService.addHero({ name } as Hero)
-      .subscribe(comida => {
-        this.comidas.push(comida);
-      });
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({ name } as Hero).subscribe((comida) => {
+      this.comidas.push(comida);
+    });
   }
 
   delete(comida: Hero): void {
-    this.comidas = this.comidas.filter(c => c !== comida);
+    this.comidas = this.comidas.filter((c) => c !== comida);
     this.heroService.deleteHero(comida.id).subscribe();
   }
 
   deleteMenu(menu: Menu): void {
-    this.menus = this.menus.filter(m => m !== menu);
+    this.menus = this.menus.filter((m) => m !== menu);
     this.heroService.deleteHero(menu.idMenu).subscribe();
   }
 }
--- src/app/rocket-delivery.service.ts
+++ src/app/rocket-delivery.service.ts
@@ -1,57 +1,55 @@
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { LoginResponse } from "./login-response";
-import { Menu } from "./menu";
-import { Contacto } from "./contacto";
-import { UtilService } from "./util-service";
+import { LoginResponse } from './login-response';
+import { Menu } from './menu';
+import { Contacto } from './contacto';
+import { UtilService } from './util-service';
 
 @Injectable({ providedIn: 'root' })
 export class RocketDeliveryService {
+  private rocketDeliveryUrl = 'http://localhost:8080/'; // URL to web api
 
-    private rocketDeliveryUrl = 'http://localhost:8080/';  // URL to web api
+  constructor(private http: HttpClient, private utilService: UtilService) {}
 
-    constructor(
-        private http: HttpClient,
-        private utilService: UtilService
-    ) { }
-     //listar menu   
-    getMenus(): Observable<Menu[]> {
-        return this.http.get<Menu[]>(this.rocketDeliveryUrl + 'api/listar-menus',
-            {
-                headers: new HttpHeaders({
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + this.utilService.getToken()
-                })
-            });
-    }
-    //listar contactos
-    getContactos(): Observable<Contacto[]> {
-        return this.http.get<Contacto[]>(this.rocketDeliveryUrl + 'api/listar-contactos',
-            {
-                headers: new HttpHeaders({
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + this.utilService.getToken()
-                })
-            });
-    }
+  // login
+  login(username: string, password: string): Observable<LoginResponse> {
+    const body = new HttpParams()
+      .set('username', username)
+      .set('password', password);
 
+    return this.http.post<LoginResponse>(
+      this.rocketDeliveryUrl + 'login',
+      body.toString(),
+      {
+        headers: new HttpHeaders().set(
+          'Content-Type',
+          'application/x-www-form-urlencoded'
+        ),
+      }
+    );
+  }
 
+  // listar menu
+  getMenus(): Observable<Menu[]> {
+    return this.http.get<Menu[]>(this.rocketDeliveryUrl + 'api/listar-menus', {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + this.utilService.getToken(),
+      }),
+    });
+  }
 
-
-
-
-
-    login(username: string, password: string): Observable<LoginResponse> {
-        const body = new HttpParams()
-            .set('username', username)
-            .set('password', password);
-
-        return this.http.post<LoginResponse>(this.rocketDeliveryUrl + 'login',
-            body.toString(),
-            {
-                headers: new HttpHeaders()
-                    .set('Content-Type', 'application/x-www-form-urlencoded')
-            });
-    }
+  // listar contactos
+  getContactos(): Observable<Contacto[]> {
+    return this.http.get<Contacto[]>(
+      this.rocketDeliveryUrl + 'api/listar-contactos',
+      {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + this.utilService.getToken(),
+        }),
+      }
+    );
+  }
 }
