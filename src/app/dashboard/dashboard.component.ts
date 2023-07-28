817a6f679fa3514f3933fe5f1b17ce2d6e2e6fa1
dubraska31 <dubraskaprimi31@gmail.com>
2023-07-28 18:16:27 -0300

    estilos carrito

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -43,7 +43,7 @@ export class DashboardComponent implements OnInit {
 
     if (confirmarAgregar) {
       this.utilService.agregarComidaCarrito(menu);
-      alert('Agregado al carrito!');
+
     }
   }
 
